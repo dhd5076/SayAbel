4533522ca7dd283c5eaac2e597855c6efbc3dfc9
Dylan Dunn <32021277+dhd5076@users.noreply.github.com>
2017-10-11 16:20:50 -0400

    Improved graphics and basic input

--- experiments/basic_visualization.py
+++ experiments/basic_visualization.py
@@ -22,11 +22,11 @@ class NeuralNet:
 
     def __init__(self):
         self.neurons = []
-        neurons_per_layer = 6
-        for i in range(0,72):
+        neurons_per_layer = 12
+        for i in range(0,144):
             self.neurons.append({
-                'X': 250 + (50 * (i // neurons_per_layer)),
-                'Y': 300 + (-50 * (i % neurons_per_layer)),
+                'X': 200 + (64 * (i // neurons_per_layer)),
+                'Y': 400 + (-32 * (i % neurons_per_layer)),
                 'LAYER': (i // neurons_per_layer),
                 'POSITION': (i % neurons_per_layer),
                 'VALUE': 0,
@@ -35,12 +35,13 @@ class NeuralNet:
         for neuron in self.neurons:
             for i in range(len(self.neurons)):
                 if neuron['LAYER'] == self.neurons[i]['LAYER'] + 1:
-                    neuron['CONNECTIONS'].append([i, random.random()])
-        #self.neurons[0]['VALUE'] = 0
+                    neuron['CONNECTIONS'].append([i, random.uniform(-1,1)])
+        #create observance
+        self.neurons.append({})
 
 
 def draw_square(x,y,value):
-    glColor3f(0, 0, value)
+    glColor3f(1 - value, 0, value)
 
     glVertex2i(x, y)
     glVertex2i(x + 10, y)
@@ -54,12 +55,15 @@ def draw_square(x,y,value):
     glVertex2i(x, y + 10)
     glVertex2i(x, y)
 
+
+def tan_h(x):
+    return tanh(x)
 
 def sig(x):
     return 1 / (1 + exp(-x))
 
 def draw_line(x1, y1, x2, y2, value):
-    glColor3f(value / 2, value / 2, 0)
+    glColor3f(value, 1 - (value - 1), 0)
     glVertex2i(x1, y1)
     glVertex2i(x2, y2)
 
@@ -72,8 +76,13 @@ fps_display = pyglet.clock.ClockDisplay(format='%(fps).2f fps')
 def on_key_press(symbol, modifiers):
     pass #on_draw()
 
+
+time = 0
+
+
 @window_instance.event
 def on_draw():
+    delta_time = 0
     glClear(GL_COLOR_BUFFER_BIT)
     glBegin(GL_LINES)
     for neuron in net.neurons:
@@ -86,20 +95,20 @@ def on_draw():
                       conn_idx[1])
             neuron['VALUE'] += (conn_idx[1] * net.neurons[conn_idx[0]]['VALUE'])
 
-        neuron['VALUE'] = sig(neuron['VALUE']) - 0.45
+        if(len(neuron['CONNECTIONS'])):
+            neuron['VALUE'] = tan_h(neuron['VALUE'])
     glEnd()
     glBegin(GL_QUADS)
     for neuron in net.neurons:
         draw_square(neuron['X'], neuron['Y'], neuron['VALUE'])
     glEnd()
-    net.neurons[0]['VALUE'] = 1
-    #print(net.neurons[0]['VALUE'])
+    net.neurons[0]['VALUE'] = sin(time)
 
-
-#pyglet.clock.schedule(on_draw)
 
 def up(dt):
-    pass
+    global time
+    time += 0.1
+
 
 pyglet.clock.schedule_interval(up, 1.0/128.0)
 pyglet.clock.set_fps_limit(128)
