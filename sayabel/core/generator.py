31f292c167f25cefd9a84b20d53002e6bad15490
Dylan Dunn <dhd5076@users.noreply.github.com>
2017-10-19 10:31:01 -0400

    Organized texts and added build script

--- sayabel/core/generator.py
+++ sayabel/core/generator.py
@@ -2,7 +2,7 @@ from sayabel import markov
 from sayabel.core.main import red_con_instance
 
 
-def generate_title(subreddit, post_count):
+def generate_post_title(subreddit, post_count):
     post_list = red_con_instance.get_hot_posts(subreddit, post_count)
     title_chain = markov.MarkovChain()
 
@@ -11,22 +11,18 @@ def generate_title(subreddit, post_count):
 
     return title_chain.generate()
 
-def generate_custom_philo(file_name_list):
-    gen_philo_chain = markov.MarkovChain()
-    for file_name in file_name_list:
-        gen_philo_chain.train(open(file_name))
+def generate_post_text(subreddit, post_count):
+    post_list = red_con_instance.get_hot_posts(subreddit, post_count)
+    title_chain = markov.MarkovChain()
+
+    for post in post_list:
+        title_chain.train(post.text)
+
+    return title_chain.generate()
+
 
 def from_file_list(file_name_list):
     chain = markov.MarkovChain()
     for file_name in file_name_list:
         chain.train(open(file_name).read())
 
-def generate_plato():
-    text_file = open('philo/plato-phaedrus-clean')
-    text_data = text_file.read()
-    text_data += open('philo/plato-the-republic').read()
-    text_data += open('philo/plato-sophist-dirty').read()
-    output_chain = markov.MarkovChain()
-
-    output_chain.train(text_data)
-    return output_chain.generate()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = "SayAbel",
+      description="",
+      author = "Dylan Dunn",
+      author_email= "dhd5076@rit.edu",
+      url = "Put github here",
+      packages = ['sayabel'],
+      long_description="")
