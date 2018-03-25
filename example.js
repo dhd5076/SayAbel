2f9f67059ab3b1d84033a8d9256815c42b4a64a2
Dylan Dunn <dhd5076@user.noreply.github.com>
2018-03-24 22:37:12 -0400

    I'll learn how to spell nietzsche some time

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 ﻿var SayAbel  = require('./index.js')
 var sayabelInstance = new SayAbel(true);
 
-sayabelInstance.learnFromFile("data/nietzche-the-joyful-wisdom.txt", (err) => {
+sayabelInstance.learnFromFile("data/nietzsche-the-joyful-wisdom.txt", (err) => {
   sayabelInstance.generateText(100, (text, err) => {
     console.log(text);
   });
